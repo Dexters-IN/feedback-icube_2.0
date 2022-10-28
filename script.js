20694f8fbc35f02ef02f4cd9abe4b6879ffa3eeb
Dexters <113793678+Dexters-IN@users.noreply.github.com>
2022-10-28 19:27:52 +0530

    structure changed for script

--- script.js
+++ script.js
@@ -1,119 +1,55 @@
-// Init function
-$.fn.magicRatingInit = function(config) {
+      var mouth = Snap("#mouth"),
+         eyebrowL = Snap('#eyebrowL'),
+         eyebrowR = Snap('#eyebrowR'),
+         items = document.querySelectorAll('[data-reaction]'),
+         eyebrowPathLeft,
+         eyebrowPathRight,
+         mouthPath;
 
-    // Init each widget return by the selector
-    for (widget of $(this)) {
-        var magicRatingWidget = $(widget);
-        //// Get datas ////
-        // Icon +
-        if (magicRatingWidget.data("iconGood") == null) {
-            magicRatingWidget.data("iconGood", config.iconGood != null ? config.iconGood : "fa-star");
-        };
+      for (var i = 0; i < items.length; i++) {
+         items[i].addEventListener('click', function() {
+            switchReaction(this.getAttribute('data-reaction'));
+         });
+      }
 
-        // Icon -
-        if (magicRatingWidget.data("iconBad") == null) {
-            magicRatingWidget.data("iconBad", config.iconBad != null ? config.iconBad : "fa-star-o");
-        };
+      function switchReaction(reaction) {
+         switch (reaction) {
+            case 'smile':
+               mouthPath = 'M90,80 C100,124 144,124 155,80 C144,80 100,80 90,80';
+               eyebrowPathLeft = 'M10,28 C20,4 46,4 56,28';
+               eyebrowPathRight = 'M189,28 C199,4 223,4 233,28';
+               break;
+            case 'less-smile':
+               mouthPath = 'M90,80 C100,124 144,124 155,80 C144,124 100,124 90,80';
+               eyebrowPathLeft = 'M10,28 C20,4 46,4 56,28';
+               eyebrowPathRight = 'M189,28 C199,4 223,4 233,28';
+               break;
+            case 'serious':
+               mouthPath = 'M90,110 C100,110 144,110 155,110 C144,110 100,110 90,110';
+               eyebrowPathLeft = 'M14,30 56,8';
+               eyebrowPathRight = 'M189,28 232,28';
+               break;
+            case 'less-angry':
+               mouthPath = 'M90,110 C100,80 144,80 155,110 C144,80 100,80 90,110';
+               eyebrowPathLeft = 'M10,28 C20,4 46,4 56,28';
+               eyebrowPathRight = 'M189,28 C199,4 223,4 233,28';
+               break;
+            case 'angry':
+               mouthPath = 'M90,110 C100,110 144,110 155,110 C144,110 100,110 90,110';
+               eyebrowPathLeft = 'M14,8 56,30';
+               eyebrowPathRight = 'M189,28 232,6';
+               break;
+         }
 
-        // Max mark
-        if (magicRatingWidget.data("maxMark") == null) {
-            magicRatingWidget.data("maxMark", config.maxMark != null ? config.maxMark : 5);
-        }
+         mouth.animate({
+            d: mouthPath
+         }, 600, mina.backout);
 
-        /*
-        console.log(magicRatingWidget.data("iconGood"));
-        console.log(magicRatingWidget.data("iconBad"));
-        */
+         eyebrowL.animate({
+            d: eyebrowPathLeft
+         }, 600, mina.backout);
 
-        // Clear the widget
-        magicRatingWidget.html("");
-
-        // Init icons
-        for (i = 1; i <= magicRatingWidget.data("maxMark"); i++) {
-            if (i <= magicRatingWidget.data("currentRating")) {
-                magicRatingWidget.append('<i class=" ' + magicRatingWidget.data("iconGood") + ' magic-rating-icon" aria-hidden="true" data-default=true data-rating=' + i + '></i>');
-            } else {
-                magicRatingWidget.append('<i class=" ' + magicRatingWidget.data("iconBad") + ' magic-rating-icon" aria-hidden="true" data-default=false data-rating=' + i + '></i>');
-            }
-        }
-
-        // Init reset handler
-        magicRatingWidget.on("mouseleave", function() {
-            var widget = $(this);
-            /*
-            console.log("mouseleave");
-            console.log(widget.data("iconGood"));
-            console.log(widget.data("iconBad"));
-            */
-            widget.children().each(function() {
-                var icon = $(this);
-                if (icon.data("default") && !icon.hasClass("fa-star")) {
-                    icon.removeClass(widget.data("iconBad"));
-                    icon.addClass(widget.data("iconGood"));
-                } else if (!icon.data("default") && !icon.hasClass("fa-star-o")) {
-                    icon.removeClass(widget.data("iconGood"));
-                    icon.addClass(widget.data("iconBad"));
-                }
-            });
-        });
-
-        // Init click handler
-        magicRatingWidget.on("click", ".magic-rating-icon", function() {
-            // Get rating
-            var icon = $(this);
-            var widget = icon.parent();
-            var rating = icon.data("rating");
-            /*
-            console.log("click");
-            console.log(widget.data("iconGood"));
-            console.log(widget.data("iconBad"));
-            */
-            // Update rating
-            widget.children().each(function() {
-                if ($(this).data("rating") <= rating) {
-                    if (!$(this).hasClass(widget.data("iconGood"))) {
-                        $(this).removeClass(widget.data("iconBad"));
-                        $(this).addClass(widget.data("iconGood"));
-                    };
-                    $(this).data("default", true);
-                } else {
-                    if (!$(this).hasClass(widget.data("iconBad"))) {
-                        $(this).removeClass(widget.data("iconGood"));
-                        $(this).addClass(widget.data("iconBad"));
-                    }
-                    $(this).data("default", false);
-                }
-            });
-
-            var callbackSuccess = config.success.bind(null, widget, rating);
-            callbackSuccess();
-        });
-
-        // Init hover icons
-        magicRatingWidget.on("mouseenter", ".magic-rating-icon", function() {
-            var icon = $(this);
-            var rating = icon.data("rating");
-            var widget = icon.parent();
-            /*
-            console.log("mouseenter");
-            console.log(widget.data("iconGood"));
-            console.log(widget.data("iconBad"));
-            */
-            widget.children().each(function() {
-                if ($(this).data("rating") <= rating) {
-                    if (!$(this).hasClass(widget.data("iconGood"))) {
-                        $(this).removeClass(widget.data("iconBad"));
-                        $(this).addClass(widget.data("iconGood"));
-                    };
-                } else {
-                    if (!$(this).hasClass(widget.data("iconBad"))) {
-                        $(this).removeClass(widget.data("iconGood"));
-                        $(this).addClass(widget.data("iconBad"));
-                    }
-                }
-            });
-        });
-    }
-};
-
-// Update for hover icons
+         eyebrowR.animate({
+            d: eyebrowPathRight
+         }, 600, mina.backout);
+      }
